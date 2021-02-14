95ab61647bc0b58694877e949fb278c56978a31c
Mayank Singh <mayank.singh@crio-users.in>
2021-02-14 23:35:31 +0530

    c

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
 const path = require("path");
 
-dotenv.config({ path: path.join(__dirname, "../../.env") });
+dotenv.config({ path: "./.env" });
+
 module.exports = {
   port: process.env.PORT,
   mongoose: {
